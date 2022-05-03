f1a027d1620da425f961b026565d8a35381a5bc5
Andrew Smith <andrew.smith.dev209@gmail.com>
2022-05-03 09:42:28 +0500

    change url again'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -31,7 +31,7 @@ export default {
   data: () => ({
     link: "https://discord.com/api/oauth2/authorize?client_id=970534833753292871&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fredirect&response_type=code&scope=identify%20guilds",
     twitterLink:
-      "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=bXhNTGkxVnIwYTRlSnFiS3plYTA6MTpjaQ&redirect_uri=https://6270af22157aed6f8a4cf493--effulgent-souffle-d42e2d.netlify.app&scope=tweet.read%20users.read%20follows.read%20follows.write&state=twitter&code_challenge=challenge&code_challenge_method=plain",
+      "https://twitter.com/i/oauth2/authorize?response_type=code&client_id=bXhNTGkxVnIwYTRlSnFiS3plYTA6MTpjaQ&redirect_uri=https://main--effulgent-souffle-d42e2d.netlify.app&scope=tweet.read%20users.read%20follows.read%20follows.write&state=twitter&code_challenge=challenge&code_challenge_method=plain",
     discordData: JSON.parse(window.localStorage.getItem("discordUser")) || {},
   }),
   computed: {},
